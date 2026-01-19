de3455e93f7c6c02675094cd36f019261611a7f5
SDQ-BQA <sdmbqa420@gmail.com>
2026-01-20 01:40:26 +0530

    first(1.1) commit with basic configuration 20-01-2k26 01:44

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { AiFillApple } from "react-icons/ai";
+import "./App.css";
+export const App = () => {
+  return (
+    <div className="text-3xl font-bold underline">
+      <div className="flex items-center justify-center gap-5">
+        <span>
+          <AiFillApple />
+        </span>
+        <span>Hello Friends!!!!!!!!</span>
+      </div>
+    </div>
+  );
+};
